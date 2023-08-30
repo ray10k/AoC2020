4da6ed1d17306cbfb007a9f582307ae5180ed2df
Vera vd Heuvel <wouter@meewerken.com>
2023-08-30 12:19:45 +0200

    Day 1 star 2

--- src/days/day_one.rs
+++ src/days/day_one.rs
@@ -11,8 +11,10 @@ fn setup(input_path: &str) -> Vec<u32> {
     retval
 }
 
-fn first_star(numbers:Vec<u32>) -> String {
-    for start in 0..numbers.len() {
+fn first_star(numbers:&Vec<u32>) -> String {
+    assert!(numbers.len() >= 2);
+    let end = numbers.len()-1;
+    for start in 0..end {
         let one = numbers[start];
         for two in numbers[start+1..].iter() {
             let sum = one + *two;
@@ -25,15 +27,31 @@ fn first_star(numbers:Vec<u32>) -> String {
     "ERROR".to_string()
 }
 
-fn second_star()->String {
-    "".to_string()
+fn second_star(numbers:&Vec<u32>)->String {
+    assert!(numbers.len() >= 3);
+    let end = numbers.len()-2;
+    for first in 0..end {
+        let one = numbers[first];
+        for second in first+1 .. end+1{
+            let two = numbers[second];
+            for three in numbers[second+1 ..].iter(){
+                let sum = one + two + *three;
+                if sum == 2020 {
+                    let product = one * two * *three;
+                    return product.to_string();
+                }
+            }
+        }
+    }
+
+    "ERROR".to_string()
 }
 
 pub fn run_day(input_path: &str) {
     let numbers = setup(input_path);
     println!("Parsed number count: {}",numbers.len());
-    let star_one = first_star(numbers);
-    let star_two = second_star();
+    let star_one = first_star(&numbers);
+    let star_two = second_star(&numbers);
     println!("AoC 2020, day 1.\nStar 1 output: {star_one}.\nStar 2 output: {star_two}.");
 
 }
