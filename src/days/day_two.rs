ad9d16e164e31287821515e4d06a8becd8122d2b
Vera vd Heuvel <wouter@meewerken.com>
2023-08-30 14:27:45 +0200

    Day 2 star 1

---
+++ src/days/day_two.rs
@@ -0,0 +1,62 @@
+use std::fs;
+use std::convert::TryFrom;
+
+struct PasswordLine{
+    min:usize,
+    max:usize,
+    character:String,
+    password:String,
+}
+
+impl TryFrom<&str> for PasswordLine {
+    type Error = &'static str;
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        //check format
+        if value.matches('-').count() != 1 {
+            return Err("format mismatch; exactly 1 - is expected.");
+        }
+        if value.matches(' ').count() != 2 {
+            return Err("format mismatch; exactly 2 spaces are expected.");
+        }
+        if value.matches(':').count() != 1 {
+            return Err("format mismatch; exactly 1 : is expected.");
+        }
+        let parts: Vec<&str> = value.split([' ',':','-'].as_ref()).collect();
+        println!("{:?}",parts);
+        let min:usize = parts[0].parse().unwrap();
+        let max:usize = parts[1].parse().unwrap();
+        let character = parts[2].to_owned();
+        let password = parts[4].to_owned();
+        return Ok(PasswordLine { min, max: max, character: character, password: password });
+    }
+}
+
+fn setup<'a>(input_path:&str) -> Vec<PasswordLine>{
+    let mut retval:Vec<PasswordLine> = Vec::<PasswordLine>::new();
+    let input_content = fs::read_to_string(input_path).unwrap();
+    for line in input_content.lines() {
+        if let Ok(pass_line) = PasswordLine::try_from(line) {
+            retval.push(pass_line);
+        }
+    }
+
+    retval
+}
+
+fn first_star(input:&Vec<PasswordLine>) -> String {
+    let mut correct_count:u32 = 0;
+    for pw in input{
+        let letter_count = pw.password.matches(pw.character.chars().take(1).last().unwrap()).count();
+        if letter_count >= pw.min && letter_count <= pw.max {
+            correct_count += 1;
+        }
+    }
+    return correct_count.to_string();
+}
+
+pub fn run_day(input_path:&str) {
+    let data = setup(input_path);
+    let first = first_star(&data);
+
+    println!("Day 2.\nStar one: {first}.\nStar two: .");
+}
--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,5 @@ mod days;
 fn main() {
     println!("Hello, world!");
     days::day_one::run_day("inputs/day_1.txt");
+    days::day_two::run_day("inputs/day_2.txt");
 }
